c154e49101d582491bd42b63daa3e3dc84bd5b7d
Angel Alvarez Castro <angel.alvarez@quadit.mx>
2020-01-14 15:27:20 -0600

    [FIX], correccion en campos para generar el envio

--- tipsa_etiqueta/wizard/ws_etiqueta.py
+++ tipsa_etiqueta/wizard/ws_etiqueta.py
@@ -241,9 +241,9 @@ class ws_etiqueta(models.Model):
         _logger.info("======> Direccion %r" % self.DirDes)
         _logger.info("======> Nombre %r" % self.NomDes)
         nombre = str(self.NomDes)
-        nombre_destino = nombre.decode('latin-1')
+        nombre_destino = nombre.encode('latin-1')
         direccion = str(self.DirDes)
-        direccion_destino = direccion.decode('latin-1')
+        direccion_destino = direccion.encode('latin-1')
         _logger.info("======> Direccioooon %r" % direccion_destino)
         _logger.info("======> Nombreeee %r" % nombre_destino)
 
